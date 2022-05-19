4343607b1636cb57535142f74928273a506798e5
dwstrong5 <dstrong230@gmail.com>
2022-05-18 18:57:08 -0700

    completed js exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0 || isNaN(a) || isNaN(b)) {
+        return "ERROR";
+    }
+
+    if (!(typeof(a) === 'number' && typeof(b) === 'number')) {
+        return "ERROR";
+    }
+
+    result = 0;
+
+    if (a === b) {
+        return a;
+    }
+
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            result += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++) {
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
