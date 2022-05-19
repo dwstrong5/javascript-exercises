7d4547e718f66e19fbdb9c9492a15bb397727bca
dwstrong5 <dstrong230@gmail.com>
2022-05-18 18:14:38 -0700

    completed js exercises 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(s, n) {
+
+if(n < 0) { return "ERROR" }
+
+    result = "";
+    for (let i = 0; i < n; i++) {
+        result+=s;
+    }
+
+    return result;
 
 };
 
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(s) {
+	
+	result = "";
+	for (let i = s.length - 1; i >= 0; i--) {
+		result += s.charAt(i);
+	}
 
+return result;
 };
 
 // Do not edit below this line
