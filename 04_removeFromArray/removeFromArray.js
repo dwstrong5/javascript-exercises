6f7206f6de3bb1ad67f4d92c8be1c48fe78a4450
dwstrong5 <dstrong230@gmail.com>
2022-05-18 18:42:23 -0700

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+
+    if (args.length < 2) {
+        return "ERROR";
+    }
+
+    let arr = args[0];
+
+    for (let i = 1; i < args.length; i++) {
+        
+        for (let j = 0; j < arr.length; j++) {
+            if(arr[j] === args[i]) {
+                arr.splice(j,1);
+            }
+        }
+    }
+    return arr;
 
 };
 
