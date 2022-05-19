1e73e730324da316f61ba1b98c9534fdf4b94ae9
dwstrong5 <dstrong230@gmail.com>
2022-05-18 19:20:51 -0700

    finished js exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(t) {
+  temp = (t - 32.0) * (5.0/9.0);
+  return Math.round(temp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(t) {
+  temp = (t * (9.0/5.0)) + 32.0;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
